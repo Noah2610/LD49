d70f88cf9f253ce28898ef27e384ae23650f9fea
Noah Rosenzweig <rosenzweig.noah@gmail.com>
2021-10-02 16:38:23 +0200

    Add background image, update animation stuff

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,22 @@
 import "./styles/index.scss";
 
-import devImg from "./assets/dev.png";
+import devImg from "./assets/spr_test_B.png";
+import bgImg from "./assets/bg/bg_test_B.png";
+
 import { createSpritesheet } from "./spritesheet";
 import { expectEl } from "./util";
 import { createAnimation } from "./animation";
+import { createAnimationContainer } from "./animation/createAnimationContainer";
 
 function main() {
     dev();
+
+    setupBg();
+}
+
+async function setupBg() {
+    const bgEl = expectEl("#game #background");
+    bgEl.style.backgroundImage = `url(${bgImg})`;
 }
 
 async function dev() {
@@ -15,38 +25,65 @@ async function dev() {
 
     const spritesheet = await createSpritesheet(devImg, {
         spriteSize: {
-            w: 16,
-            h: 16,
+            w: 48,
+            h: 80,
         },
+        // 0.6
+        // 1.666667
         size: {
-            w: 64,
-            h: 64,
+            w: 400.0,
+            h: 666,
         },
-        currentIdx: 2,
+        currentIdx: 0,
     });
 
     const animation = createAnimation(spritesheet, {
         loop: true,
         frames: [
-            [0, 500],
-            [1, 500],
-            [0, 500],
-            [1, 500],
-            [0, 2000],
-            [4, 250],
-            [5, 250],
-            [4, 250],
-            [5, 250],
-            [4, 250],
-            [5, 250],
-            [4, 250],
-            [5, 250],
-            [4, 250],
-            [5, 250],
+            [0, 130],
+            [1, 130],
+            [2, 130],
+            [3, 130],
+            [4, 130],
+            [5, 130],
+            [6, 130],
+            [7, 130],
         ],
     });
 
-    animation.play();
+    // animation.play();
+
+    const animationContainer = createAnimationContainer(spritesheet, {
+        defaultAnimation: "default",
+        animations: {
+            default: {
+                loop: true,
+                frames: [
+                    [0, 130],
+                    [1, 130],
+                    [2, 130],
+                    [3, 130],
+                    [4, 130],
+                    [5, 130],
+                    [6, 130],
+                    [7, 130],
+                ],
+            },
+            other: {
+                loop: false,
+                frames: [
+                    [4, 500],
+                    [1, 500],
+                    [4, 500],
+                    [1, 500],
+                ],
+            },
+        },
+    });
+
+    animationContainer.play("default");
+
+    // setTimeout(() => animationContainer.play("other"), 2000);
 
     // @ts-ignore
     window.DBG = { spritesheet, animation };
