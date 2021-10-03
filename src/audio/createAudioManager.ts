7aa336342fee54d7a5291331f8d2cfc551ec166c
Noah Rosenzweig <rosenzweig.noah@gmail.com>
2021-10-03 22:30:31 +0200

    Only set audioBgm.currentlyPlaying if play was successful
    This makes the update loop continue trying to play the current emotion's
    track, until it worked (until user has interacted with page).

--- src/audio/createAudioManager.ts
+++ src/audio/createAudioManager.ts
@@ -32,8 +32,13 @@ function createAudioBgm(config: AudioBgmConfig): AudioBgm {
             return;
         }
         bgm.stop();
-        audio.play();
-        bgm.currentlyPlaying = name;
+        audio.play().then(
+            () => (bgm.currentlyPlaying = name),
+            () =>
+                console.error(
+                    `[bgm.play] Couldn't play audio, user probably hasn't interacted with page yet`,
+                ),
+        );
     };
 
     const stop: AudioBgm["stop"] = () => {
@@ -58,8 +63,13 @@ function createAudioBgm(config: AudioBgmConfig): AudioBgm {
         const currentTime = bgm.getCurrentlyPlaying()?.currentTime ?? 0;
         bgm.stop();
         audio.currentTime = currentTime;
-        audio.play();
-        bgm.currentlyPlaying = name;
+        audio.play().then(
+            () => (bgm.currentlyPlaying = name),
+            () =>
+                console.error(
+                    `[bgm.switchTo] Couldn't play audio, user probably hasn't interacted with page yet`,
+                ),
+        );
     };
 
     const getCurrentlyPlaying: AudioBgm["getCurrentlyPlaying"] = () => {
