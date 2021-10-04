fea97552b18629be7bb2bd472e25b20881bdc7d3
hoichael <31223078+hoichael@users.noreply.github.com>
2021-10-04 19:26:09 +0200

    Strip item config default action of everything except MoodChange

--- src/config/item.ts
+++ src/config/item.ts
@@ -147,30 +147,10 @@ export const ITEM_CONFIG: ItemConfig = {
                 type: "Actions",
                 actions: [
                     {
-                        type: "Debug",
-                        message: "TOY!",
-                    },
-                    {
                         type: "MoodChange",
                         velocity: {
                             min: 5,
                             max: 10,
-                        },
-                    },
-                    {
-                        type: "AddText",
-                        randomText: [
-                            "Life can be fun! ",
-                            "Let's play! ",
-                            "Play with me! ",
-                            "You want this? ",
-                        ],
-                    },
-                    {
-                        type: "SpawnSpeechBubble",
-                        randomText: ["Yeah!", "Let's go! ", "This'll be fun! "],
-                        options: {
-                            despawnMs: 2000,
                         },
                     },
                 ],
@@ -261,31 +241,10 @@ export const ITEM_CONFIG: ItemConfig = {
                 type: "Actions",
                 actions: [
                     {
-                        type: "Debug",
-                        message: "SYRINGE!",
-                    },
-                    {
                         type: "MoodChange",
                         velocity: {
                             min: -10,
                             max: -5,
-                        },
-                    },
-                    {
-                        type: "AddText",
-                        randomText: [
-                            "This should calm him down.",
-                            "Take this.",
-                            "Cool down.",
-                            "Take a rest.",
-                            "This'll make you relax.",
-                        ],
-                    },
-                    {
-                        type: "SpawnSpeechBubble",
-                        randomText: ["Drugs!", "No more.", "Don't do it!"],
-                        options: {
-                            despawnMs: 2000,
                         },
                     },
                 ],
