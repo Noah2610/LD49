4f34cafbfc6832f4c6c503bf5b5ec6d3202fcd27
hoichael <31223078+hoichael@users.noreply.github.com>
2021-10-04 19:21:00 +0200

    Item config emotionActions data

--- src/config/item.ts
+++ src/config/item.ts
@@ -60,9 +60,87 @@ export const ITEM_CONFIG: ItemConfig = {
             type: "Upper",
             label: "Toy",
             emotionActions: {
+                Suicidal: {
+                    type: "Actions",
+                    actions: [
+                        {
+                            type: "AddText",
+                            randomText: [
+                                "Wakey wakey, sunshine! ",
+                                "Now now, don't be such a baby... ",
+                            ],
+                        },
+                    ],
+                },
+                Depressed: {
+                    type: "Actions",
+                    actions: [
+                        {
+                            type: "AddText",
+                            randomText: ["Okok... happy now? "],
+                        },
+                    ],
+                },
+                Sad: {
+                    type: "Actions",
+                    actions: [
+                        {
+                            type: "AddText",
+                            randomText: ["Okok... happy now? "],
+                        },
+                    ],
+                },
                 Calm: {
-                    type: "Debug",
-                    message: "LMAO",
+                    type: "Actions",
+                    actions: [
+                        {
+                            type: "AddText",
+                            randomText: [
+                                "You seem exceptionally stable and healthy. How boring... ",
+                            ],
+                        },
+                    ],
+                },
+                Happy: {
+                    type: "Actions",
+                    actions: [
+                        {
+                            type: "AddText",
+                            randomText: [
+                                "No such thing as too much happiness, amirite? ",
+                                "What could go wrong? ",
+                                "Let's take it up a notch, shall we? ",
+                            ],
+                        },
+                    ],
+                },
+                Excited: {
+                    type: "Actions",
+                    actions: [
+                        {
+                            type: "AddText",
+                            randomText: [
+                                "No need to worry - I'm a professional! ",
+                                "What? What about your liver? It'll grow back. ",
+                                `What do you mean, "yOu'Re aBoUt To eXpLoDe"? `,
+                                "Don't worry, bro. The ladies dig guys with acute heart failure ",
+                            ],
+                        },
+                    ],
+                },
+                Manic: {
+                    type: "Actions",
+                    actions: [
+                        {
+                            type: "AddText",
+                            randomText: [
+                                "Don't worry, it's just chemicals bro ",
+                                "Well, here goes nothing... ",
+                                "Can't have too much of a good thing! ...right? ",
+                                "Ok mate, now take a deeeeeeep breath... ",
+                            ],
+                        },
+                    ],
                 },
             },
             action: {
@@ -82,15 +160,15 @@ export const ITEM_CONFIG: ItemConfig = {
                     {
                         type: "AddText",
                         randomText: [
-                            "Life can be fun!",
-                            "Let's play!",
-                            "Play with me!",
-                            "You want this?",
+                            "Life can be fun! ",
+                            "Let's play! ",
+                            "Play with me! ",
+                            "You want this? ",
                         ],
                     },
                     {
                         type: "SpawnSpeechBubble",
-                        randomText: ["Yeah!", "Let's go!", "This'll be fun!"],
+                        randomText: ["Yeah!", "Let's go! ", "This'll be fun! "],
                         options: {
                             despawnMs: 2000,
                         },
@@ -101,6 +179,84 @@ export const ITEM_CONFIG: ItemConfig = {
         Downer: {
             type: "Downer",
             label: "Syringe",
+            emotionActions: {
+                Suicidal: {
+                    type: "Actions",
+                    actions: [
+                        {
+                            type: "AddText",
+                            randomText: [
+                                "Don't worry, it's just chemicals bro ",
+                                "I am become death, conductor of irresponsible experiments ",
+                                `"Risk breeds innovation" - Someone, probably `,
+                                `"No risk no progress" - or something like that...`,
+                                "Your sacrifice will not be in vain! ...ok I lied. ",
+                                "What was his name again? -Uh, i mean, what is your name again? Paperwork, y'know... Hello? ",
+                            ],
+                        },
+                    ],
+                },
+                Depressed: {
+                    type: "Actions",
+                    actions: [
+                        {
+                            type: "AddText",
+                            randomText: ["No risk no fun", "Oops... "],
+                        },
+                    ],
+                },
+                Sad: {
+                    type: "Actions",
+                    actions: [
+                        {
+                            type: "AddText",
+                            randomText: ["Oh, don't give me that look... "],
+                        },
+                    ],
+                },
+                Calm: {
+                    type: "Actions",
+                    actions: [
+                        {
+                            type: "AddText",
+                            randomText: [
+                                "This might sting a little ",
+                                "Okok but... why not? ",
+                            ],
+                        },
+                    ],
+                },
+                Happy: {
+                    type: "Actions",
+                    actions: [
+                        {
+                            type: "AddText",
+                            randomText: [],
+                        },
+                    ],
+                },
+                Excited: {
+                    type: "Actions",
+                    actions: [
+                        {
+                            type: "AddText",
+                            randomText: [
+                                "Let's not get TOO excited, m'kay? ",
+                                "FINEEEE... you're such a buzzkill.. ",
+                            ],
+                        },
+                    ],
+                },
+                Manic: {
+                    type: "Actions",
+                    actions: [
+                        {
+                            type: "AddText",
+                            randomText: ["Let's cool down a little, ye? "],
+                        },
+                    ],
+                },
+            },
             action: {
                 type: "Actions",
                 actions: [
