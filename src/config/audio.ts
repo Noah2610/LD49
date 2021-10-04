f9f82a9cec0db566f2980987c5b7bda6ccc20816
Noah Rosenzweig <rosenzweig.noah@gmail.com>
2021-10-04 19:20:49 +0200

    Add SFX configs

--- src/config/audio.ts
+++ src/config/audio.ts
@@ -2,31 +2,59 @@ import { AudioFileConfig, AudioManagerConfig } from "../audio";
 
 const genFileConfig = (
     name: string,
+    type: "bgm" | "sfx",
     opts?: Partial<AudioFileConfig>,
 ): AudioFileConfig => ({
     name,
-    src: require(`../assets/audio/bgm/${name}.ogg`),
-    volume: 0.1,
+    src: require(`../assets/audio/${type}/${name}.ogg`),
     ...opts,
 });
+
+const genBgm = (
+    name: string,
+    opts?: Partial<AudioFileConfig>,
+): AudioFileConfig =>
+    genFileConfig(name, "bgm", {
+        volume: 0.5,
+        loop: true,
+        ...opts,
+    });
+
+const genSfx = (
+    name: string,
+    opts?: Partial<AudioFileConfig>,
+): AudioFileConfig =>
+    genFileConfig(name, "sfx", {
+        volume: 1.0,
+        loop: false,
+        ...opts,
+    });
 
 export const AUDIO_CONFIG: AudioManagerConfig = {
     bgm: {
         files: [
-            genFileConfig("happy1", { loop: true }),
-            genFileConfig("happy2", { loop: true }),
-            genFileConfig("sad1", { loop: true }),
-            genFileConfig("sad2", { loop: true }),
-            genFileConfig("sad3", { loop: true }),
+            genBgm("happy1"),
+            genBgm("happy2"),
+            genBgm("sad1"),
+            genBgm("sad2"),
+            genBgm("sad3"),
         ],
     },
 
     sfx: {
         files: [
-            {
-                name: "textbox-char",
-                src: require("../assets/audio/sfx/neutralspeech.ogg"),
-            },
+            genSfx("crying"),
+            genSfx("happy2speech", { name: "text-happy2" }),
+            genSfx("happyspeech", { name: "text-happy1" }),
+            genSfx("manicspeech1", { name: "text-manic1" }),
+            genSfx("manicspeech2", { name: "text-manic2" }),
+            genSfx("manicspeech3", { name: "text-manic3" }),
+            genSfx("neutralspeech", { name: "text-calm1" }),
+            genSfx("randomclick"),
+            genSfx("sad1speech", { name: "text-sad1" }),
+            genSfx("sad2speech", { name: "text-sad2" }),
+            genSfx("suicidalspeech", { name: "text-suicidal1" }),
+            genSfx("syringe"),
         ],
     },
 };
